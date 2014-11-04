232e87831c84fb7d9590f4bef4b9f80ddd8f4399
sraj <sanjayrajsoni@sanjays-MacBook-Air.local>
2014-11-03 21:28:39 -0500

    initial commit

---
+++ src/hello.erl
@@ -0,0 +1,24 @@
+%% Author: sanjayrajsoni
+%% Created: May 30, 2012
+%% Description: TODO: Add description to hello
+-module(hello).
+
+%%
+%% Include files
+%%
+
+%%
+%% Exported Functions
+%%
+-export([hello/0]).
+hello() -> io:format("Hello World!~n", []).
+%%
+%% API Functions
+%%
+
+
+
+%%
+%% Local Functions
+%%
+
